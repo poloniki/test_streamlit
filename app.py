4f566b7f96688ec26a168d78ae91b8d8452aba35
Nikita Polovinkin <poloniki@ya.ru>
2023-03-14 10:32:01 +0100

    update

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
 import streamlit as st
+from PIL import Image, ImageDraw
 import cv2
-
-
-import streamlit as st
-from PIL import Image
-
-import cv2
+from roboflow import Roboflow
 import io
 import time
 import numpy as np
 
 
 
+url = 'https://detect.roboflow.com/crowd_counting/12'
 
-def load_images(cv_image):
-    robo_prediction = cv_image
-    st.image(robo_prediction)
+
+rf = Roboflow(api_key=st.secrets['api_key'])
+project = rf.workspace().project('crowd_counting')
+model = project.version(14).model
+
+
+
+def load_images(cv_image, confidence_threshold, overlap_threshold):
+    # Make prediction using Roboflow API
+    robo_prediction = model.predict(cv_image, confidence=confidence_threshold*100, overlap=overlap_threshold*100).json()
+    st.write(f'Our system detects {len(robo_prediction["predictions"])} humans in this photo')
+    pil_img_with_boxes = Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(pil_img_with_boxes)
+
+    for prediction in robo_prediction['predictions']:
+        label = prediction['class']
+        xmin = prediction['x']
+        ymin = prediction['y']
+        width = prediction['width']
+        height =  prediction['height']
+        top_lx  = xmin  + prediction['width'] /2
+        top_ly = ymin + prediction['height'] /2
+        bottom_lx = xmin - prediction['width'] /2
+        bottom_ly = ymin - prediction['height'] /2
+        draw.rectangle(((top_lx, top_ly), (bottom_lx, bottom_ly)), outline='red', width=4)
+        draw.text((bottom_lx, bottom_ly), label, fill='white')
+    st.image(pil_img_with_boxes)
 
 
 def main():
@@ -39,7 +60,15 @@ def main():
 
         st.image(pil_image)
         if st.button("Predict Please...."):
-            load_images(cv_image)
+            with st.spinner('Detecting Humans......'):
+                time.sleep(10)
+            load_images(cv_image, confidence_threshold, overlap_threshold)
+
+        # pil_image = Image.open(io.BytesIO(r.content))
+        # cv_image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+
+        # st.image(cv_image, caption='Image with bounding boxes')
+
 
 if __name__ == '__main__':
     main()
